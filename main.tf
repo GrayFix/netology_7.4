e0abacd5a6f9602b3b952ad0542a87701a894f67
GrayFix <grayfix@gmail.com>
2021-09-04 15:34:07 +0500

    first commit

---
+++ main.tf
@@ -0,0 +1,44 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+locals {
+    web_instance_count_map = {
+        stage = 1
+        prod = 0
+    }
+}
+
+//Ищем последний дистрибутив Ubuntu 20.04
+data "aws_ami" "ubuntu" {
+    most_recent = true
+    filter {
+        name   = "name"
+        values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+    }
+    filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+    }
+    owners = ["099720109477"]   // Canonical
+}
+
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+
+//Описываем создание тестового сервера
+resource "aws_instance" "netology_test" {
+    ami = data.aws_ami.ubuntu.id   
+    instance_type = t3.micro
+    count = local.web_instance_count_map[terraform.workspace]
+
+    lifecycle {
+        create_before_destroy = true
+    }
+
+    tags = {
+        Name = "netology-test"
+    }
+}
+
+
